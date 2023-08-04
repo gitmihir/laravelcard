a78d49d3651d09337db0d40b7f70e045ffbf0c9c
Mihir Patel <mihirp009@gmail.com>
2023-08-03 22:36:55 -0400

    Fields

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -10,23 +10,22 @@
                             <input type="hidden" name="token" value="{{ $request->route('token') }}">
                             <div class="block">
                                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                <x-jet-input id="email" class="form-control" type="email" name="email"
                                     :value="old('email', $request->email)" required autofocus />
                             </div>
                             <div class="mt-4">
                                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password"
+                                <x-jet-input id="password" class="form-control" type="password" name="password"
                                     required autocomplete="new-password" />
                             </div>
                             <div class="mt-4">
                                 <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                                <x-jet-input id="password_confirmation" class="form-control" type="password"
                                     name="password_confirmation" required autocomplete="new-password" />
                             </div>
                             <div class="flex items-center justify-end mt-4">
-                                <x-jet-button>
-                                    {{ __('Reset Password') }}
-                                </x-jet-button>
+                                <button class="btn btn-primary" type="submit"> {{ __('Reset Password') }}
+                                </button>
                             </div>
                         </form>
                     </div>
