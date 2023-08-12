ff19f253c07f4eeb43e91c05c54cd17b818fc1f4
Mihir Patel <mihirp009@gmail.com>
2023-08-12 19:20:53 -0400

    dasdasd

--- resources/views/frontwebsite/paysuccess.blade.php
+++ resources/views/frontwebsite/paysuccess.blade.php
@@ -9,7 +9,6 @@
                         $payment_id = $_GET['payment_id'];
                         $product_id = $_GET['product_id'];
                         $curl = curl_init();
-                        
                         curl_setopt_array($curl, [
                             CURLOPT_URL => 'https://api.razorpay.com/v1/payments/' . $payment_id,
                             CURLOPT_RETURNTRANSFER => true,
@@ -25,19 +24,46 @@
                         curl_close($curl);
                         $resultdata = json_decode($response);
                         $authorized = $resultdata->status;
-                        echo '<pre>';
-                        print_r($resultdata);
-                        echo '</pre>';
+                        $emailfromorder = $resultdata->email;
                         if ($authorized == 'authorized') {
                             App\Models\Order::where('order_id_for_status', $product_id)->update(['sg_order_status' => '1', 'payment_remark' => $payment_id]);
                             App\Models\Card::where('sg_order_id', $product_id)->update(['sg_order_status' => '1']);
+                            $viewdata = App\Models\Order::where('sg_business_email', '=', $emailfromorder)->get();
+                            $name = [];
+                            $OrderID = [];
+                            foreach ($viewdata as $userdata) {
+                                $name = $userdata->sg_full_name;
+                                $OrderID = $userdata->order_id_for_status;
+                            }
+                            $brand = Brand::all();
+                            $brandemail = [];
+                            $brandphone = [];
+                            foreach ($brand as $branddata) {
+                                $brandemail = $branddata->sg_brand_business_email;
+                                $brandphone = $branddata->sg_brand_busienss_phone;
+                            }
                         }
                     @endphp
-                    Dear $Name, we are delighted to confirm your order. Your Order ID: $OrderID. if you are an existing
-                    user you can access all updates by logging into your account. if you are a new user you can access
-                    all updates by logging into your account using provided ID and password on your registered email.
-                    Thank you for selecting us. For inquiries, kindly contact us at $BusinessPhone or email us at
-                    $BusinessEmail
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <p>
+                                    Dear {{ $name }}, we are delighted to confirm your order. Your Order
+                                    ID:{{ $OrderID }}. if you are an existing
+                                    user you can access all updates by logging into your account. if you are a new user
+                                    you can access
+                                    all updates by logging into your account using provided ID and password on your
+                                    registered email.
+                                    Thank you for selecting us. For inquiries, kindly contact us at <a
+                                        href="tel:{{ $brandphone }}">{{ $brandphone }}</a>
+                                    or
+                                    email us at
+                                    <a href="mailto:{{ $brandemail }}">{{ $brandemail }}</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
